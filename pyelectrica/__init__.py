f6d6671d3c380c0cb824b5a35451d15ed4fbe68a
Isai Aragon <36091347+x1sa1@users.noreply.github.com>
2018-12-18 19:46:40 -0600

    Delete __init__.py

--- pyelectrica/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-from pyelectrica.pyelectrica import *
-import pyelectrica.pyelectrica
-__doc__ = pyelectrica.pyelectrica.__doc__
-
-
-name =  "pyelectrica"
